7e67f93a8d92decabe3d1e0f3043887612adfce4
Pálmi Skowronski <palmis@users.noreply.github.com>
2016-01-04 19:06:55 +0100

    Basic tests for watcher

--- lib/carwatch/watch_worker.ex
+++ lib/carwatch/watch_worker.ex
@@ -55,7 +55,7 @@ defmodule CarWatch.Watcher do
   def handle_cast( {:observe, licence_plate}, state ) do
     if Map.has_key?(state, licence_plate) do
       {:ok, observations} = Map.fetch(state, licence_plate)
-      new_observations = [observations | observation(licence_plate)]
+      new_observations = [observations] ++ [observation(licence_plate)]
       {:noreply, Map.put(state, licence_plate, new_observations)}
     else
       new_observations = [observation(licence_plate)]
--- test/car_watch_test.exs
+++ test/car_watch_test.exs
@@ -1,8 +1,25 @@
 defmodule CarWatchTest do
   use ExUnit.Case
-  doctest CarWatch
-
-  test "the truth" do
-    assert 1 + 1 == 2
+  #doctest CarWatch
+  
+  setup do
+    {:ok, watcher} = CarWatch.Watcher.start_link([])
+    {:ok, watcher: watcher}
+  end
+  
+  test "observe merci", %{watcher: watcher} do
+    assert CarWatch.Watcher.observe(watcher, "merci") == :ok
+  end
+  
+  test "recollect onion", %{watcher: watcher} do
+    assert CarWatch.Watcher.recollect(watcher, "onion") == :error
+    
+    assert CarWatch.Watcher.observe(watcher, "onion") == :ok
+    {:ok, observations} = CarWatch.Watcher.recollect(watcher, "onion")
+    assert  length(observations) == 1
+    
+    assert CarWatch.Watcher.observe(watcher, "onion") == :ok    
+    {:ok, observations} = CarWatch.Watcher.recollect(watcher, "onion")
+    assert  length(observations) == 2
   end
 end
